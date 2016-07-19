56299ba8d3b51ea7e47b1df4b2b8fe9c5ffe3fac
jaredmichaelsmith <jared@jaredsmith.io>
2016-07-19 12:36:56 -0400

    Make map point to Min Kao

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,7 +39,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(35.958710, -83.924556));
 });
 
 function init() {
@@ -50,7 +50,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(35.958710, -83.924556), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -178,7 +178,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(35.958710, -83.924556);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
